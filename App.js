4b8d7b6ece3daf18f28f6b63c256bb52779bf640
joshraiborde <joshraiborde@gmail.com>
2021-10-08 12:02:07 -0400

    added valdiation for user

--- App.js
+++ App.js
@@ -23,3 +23,42 @@
 // }
 
 // export default App;
+
+
+function animatedForm(){
+  const arrows = document.querySelectorAll(".fa-arrow-down");
+
+  arrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      const input = arrow.previousElementSibling;
+      const parent = arrow.parentElement;
+      const nextForm = parent.nextElementSibling;
+
+      // chekc for validations
+      if(input.type  === "text" && validateUser(input))
+      nextSlide(parent, nextForm);
+    })
+  })
+}
+
+function validateUser(user){
+  if(user.value.length < 6){
+    console.log('notenoughcharacters')
+    error('rgb(189, 87, 87');
+  } else {
+    error('rgb(87, 189, 130');
+    return true;
+  }
+}
+
+function nextSlide(parent, nextForm) {
+  parent.classList.add('innactive');
+  parent.classList.remove('active');
+  nextForm.classList.add('active');
+}
+
+function error(color){
+  document.body.style.backgroundColor = color;
+}
+
+animatedForm();
