3962fcb93366a73ba0abbf9c23272c1a57b3fa97
joshraiborde <joshraiborde@gmail.com>
2021-10-09 17:27:19 -0400

    troubleshooting form

--- App.js
+++ App.js
@@ -1,42 +1,26 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 function animatedForm(){
   const arrows = document.querySelectorAll(".fa-arrow-down");
 
   arrows.forEach(arrow => {
-    arrow.addEventListener('click', () => {
+    arrow.addEventListener("click", () => {
       const input = arrow.previousElementSibling;
       const parent = arrow.parentElement;
       const nextForm = parent.nextElementSibling;
 
       // chekc for validations
-      if(input.type  === "text" && validateUser(input))
-      nextSlide(parent, nextForm);
+      if(input.type  === "text" && validateUser(input)){
+        nextSlide(parent, nextForm);
+      } else if(input.type === "email" && validateEmail(input)){
+        nextSlide(parent,nextForm)
+      } else if(input.type === 'password' && validatePassword(input)){
+        nextSlide(parent, nextForm)
+      } else {
+        parent.style.animation = "shake 0.5s ease "
+      }
+      // turn off animation
+      parent.addEventlistener("animationend", () => {
+        parent.style.animation = "";
+      } )
     })
   })
 }
@@ -44,15 +28,25 @@ function animatedForm(){
 function validateUser(user){
   if(user.value.length < 6){
     console.log('notenoughcharacters')
-    error('rgb(189, 87, 87');
+    error('rgb(189, 87, 87','notenoughcharacters');
   } else {
     error('rgb(87, 189, 130');
     return true;
   }
 }
 
+function validateEmail(email){
+  const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(validation.test(email.value)) {
+    error('rgb(87, 189, 130');
+  } else {
+    error('rgb(189, 87, 87');
+    return true;
+  }
+}
+
 function nextSlide(parent, nextForm) {
-  parent.classList.add('innactive');
+  parent.classList.add('inactive');
   parent.classList.remove('active');
   nextForm.classList.add('active');
 }
